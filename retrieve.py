022d082ea6ecb7b23fe22a4ebbe9f83d9693b22c
likehan <yzjlike0718@163.com>
2025-04-16 14:57:40 +0800

    feat:test dbpara retrieval

--- retrieve.py
+++ retrieve.py
@@ -8,9 +8,7 @@ from bert4keras.models import build_transformer_model
 from bert4keras.tokenizers import Tokenizer
 from bert4keras.snippets import sequence_padding
 
-import pandas as pd
-
-maxlen = 32
+import json
 
 # bert配置
 config_path = 'chinese_simbert_L-12_H-768_A-12/bert_config.json'
@@ -31,44 +29,25 @@ bert = build_transformer_model(
 
 encoder = keras.models.Model(bert.model.inputs, bert.model.outputs[0])
 
+# 设定最大长度
+maxlen = 128
 
-def load_data(filename):
-    data = pd.read_parquet(filename)
-    data = data.values
-    
-    D = []
-    for line in data:
-        text1, text2, label = line
-        D.append((text1, text2))
-    return D
-
-
-# 测试相似度效果
-data = load_data('datasets/lcqmc/validation-00000-of-00001-ae04bea7d65ea894.parquet')
-a_token_ids, b_token_ids = [], []
+token_ids = []
 texts = []
+# 读取数据
+with open("/home/group3/workspace/wsl/datasets/dbpara_test_4k_simplified.json", "r") as f:
+    data = json.load(f)
 
-for d in data:
-    token_ids = tokenizer.encode(d[0], max_length=maxlen)[0]
-    a_token_ids.append(token_ids)
-    token_ids = tokenizer.encode(d[1], max_length=maxlen)[0]
-    b_token_ids.append(token_ids)
-    texts.extend(d)
+for wav_id, desc in data.items():
+    token_id = tokenizer.encode(desc, max_length=maxlen)[0]
+    token_ids.append(token_id)
+    texts.append(desc)
 
-a_token_ids = sequence_padding(a_token_ids)
-b_token_ids = sequence_padding(b_token_ids)
-a_vecs = encoder.predict([a_token_ids, np.zeros_like(a_token_ids)],
-                         verbose=True)
-b_vecs = encoder.predict([b_token_ids, np.zeros_like(b_token_ids)],
-                         verbose=True)
+token_ids = sequence_padding(token_ids)
+vecs = encoder.predict([token_ids, np.zeros_like(token_ids)], verbose=True)
 
-a_vecs = a_vecs / (a_vecs**2).sum(axis=1, keepdims=True)**0.5
-b_vecs = b_vecs / (b_vecs**2).sum(axis=1, keepdims=True)**0.5
-
-
-# 测试全量检索能力
-vecs = np.concatenate([a_vecs, b_vecs], axis=1).reshape(-1, 768)
-
+vecs = vecs / (vecs**2).sum(axis=1, keepdims=True)**0.5
+vecs = vecs.reshape(-1, 768)
 
 def most_similar(text, topn=10):
     """检索最相近的topn个句子
@@ -80,6 +59,6 @@ def most_similar(text, topn=10):
     return [(texts[i], sims[i]) for i in sims.argsort()[::-1][:topn]]
 
 if __name__=="__main__":
-    result = most_similar(u'怎么开初婚未育证明', 20)
+    result = most_similar(u'中年男子的音调高亢，音量适中，语速慢慢地。', 20)
     print(result)
     
